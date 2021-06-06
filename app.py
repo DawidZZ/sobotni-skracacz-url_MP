3a235d29ad37d896e76743535754531bed270ad1
Dawid <dawid.ziobrowski@wp.pl>
2021-06-06 16:52:27 +0200

    Skracacz działa, brak bazy danych

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, redirect
-
+from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
+
+url_list = []
+id = 0
+short_url = ""
 
 @app.route("/")
 def index():
@@ -8,9 +11,20 @@ def index():
 
 @app.route("/success")
 def success():
-    return render_template("success.html")
+    return render_template("success.html", short_url = short_url, id = id)
 
 @app.route("/add", methods=["POST"])
 def add():
-    # Zrób coś aby wygenerować skrócony URL i zapamiętaj adres !
+    global id
+    id += 1
+    long_url = request.form.get('long-url')
+    url_list.append({"url": long_url, "id": id})
+
+    global short_url 
+    short_url = f"http://127.0.0.1:5000/shortener/{id}"
     return redirect("/success")
+
+@app.route("/shortener/<id>")
+def findAdress(id):
+    return redirect(url_list[int(id) - 1].get("url"))
+    
